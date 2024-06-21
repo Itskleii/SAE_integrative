eb7873671de65530046f3a81f26e46643c77ac89
kylian-iut <kadam.iut@gmail.com>
2024-06-21 13:47:57 +0200

    filtretopics

--- proj int/mqtt.py
+++ proj int/mqtt.py
@@ -28,6 +28,11 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     message = msg.payload.decode('utf-8')
     print(f"Message reçu sur le topic {msg.topic}: {message}")
+    
+    # Vérifier si le message remplit les critères attendus
+    if 'Id=' not in message or 'piece=' not in message or 'date=' not in message or ('time=' not in message and 'heure=' not in message) or 'temp=' not in message:
+        print("Message MQTT incomplet ou mal formé. Ignorer le message.")
+        return
     process_message(message)
 
 # Fonction pour traiter les messages reçus
