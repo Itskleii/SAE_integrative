60f0bc2882816b3053314bb0844d54bdea50f833
WaifuEater <l.petit723@gmail.Com>
2024-06-21 12:16:10 +0200

    amélioartion code

--- proj int/SiteCollecte/Collecte/urls.py
+++ proj int/SiteCollecte/Collecte/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'collecte'  # Définition du namespace pour l'application 'collecte'
+
 urlpatterns = [
     path('', views.home, name='home'),  # Page d'accueil avec tous les capteurs et leurs données
 ]
