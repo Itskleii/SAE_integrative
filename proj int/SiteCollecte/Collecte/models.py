d072f0b8fc81dbcbbf239de60ec46bb4eecccb5a
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-06-22 16:49:08 +0200

    changements de sensor_id et 2 3 ajustements

--- proj int/SiteCollecte/Collecte/models.py
+++ proj int/SiteCollecte/Collecte/models.py
@@ -3,8 +3,7 @@
 from django.db import models
 
 class Sensor(models.Model):
-    id = models.AutoField(primary_key=True)
-    sensor_id = models.CharField(max_length=255, unique=True)
+    sensor_id = models.CharField(max_length=255, primary_key=True)
     piece = models.CharField(max_length=255, null=True)
 
     class Meta:
@@ -14,14 +13,12 @@ class Sensor(models.Model):
         return self.sensor_id
 
 class TemperatureData(models.Model):
-    sensor_id = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
-    value = models.FloatField()
+    value = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         db_table = 'temperaturedata'
 
     def __str__(self):
-        return f"{self.sensor_id.sensor_id}, {self.timestamp}, {self.value}"
-
-
+        return f"{self.sensor.sensor_id}, {self.timestamp}, {self.value}"
