3204f3a6b65f809a51e4a362fecade7eca96141b
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-06-22 16:52:24 +0200

    Update urls.py

--- proj int/SiteCollecte/Collecte/urls.py
+++ proj int/SiteCollecte/Collecte/urls.py
@@ -8,6 +8,6 @@ app_name = 'collecte'  # Définition du namespace pour l'application 'collecte'
 urlpatterns = [
     path('', views.home, name='home'),  
     path('reset/', views.empty_sql, name='empty_sql'),  
-    path('graphiques/', views.graphiques, name='graphiques'),  # Ajout de l'URL pour les graphiques
+    path('graphiques/', views.graphiques, name='graphiques'),  
 ]
 
