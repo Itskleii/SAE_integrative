fd99be6d3490308f45da8918b1c20c645808ecec
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-06-25 12:24:15 +0200

    Update urls.py

--- proj int/SiteCollecte/Collecte/urls.py
+++ proj int/SiteCollecte/Collecte/urls.py
@@ -8,7 +8,8 @@ app_name = 'collecte'  # Définition du namespace pour l'application 'collecte'
 urlpatterns = [
     path('', views.home, name='home'),  
     path('reset/', views.empty_sql, name='empty_sql'),  
-    path('graphiques/', views.graphiques, name='graphiques'),
+    path("graphiques.html/", views.indexgraph),
+    path('api/all-sensor-data/', get_all_sensor_data, name='get_all_sensor_data'),
     path('update_sensor/<str:sensor_id>/', views.update_sensor, name='update_sensor'),
     path('view_sensor/<str:sensor_id>/', views.view_sensor, name='view_sensor'),]
 
